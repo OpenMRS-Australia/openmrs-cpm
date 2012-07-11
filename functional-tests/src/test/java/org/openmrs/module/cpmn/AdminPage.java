9b77d6ae924047ad52f8f14de7d2eca68ed80da3
David Sanders <dsanders@rapilabs.com>
2012-07-12 03:03:42 +1000

    * Adding the first functional test - for now just in src/test/java

---
+++ functional-tests/src/test/java/org/openmrs/module/cpmn/AdminPage.java
@@ -0,0 +1,42 @@
+package org.openmrs.module.cpmn;
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+public class AdminPage {
+
+	private static final String USERNAME = "Admin";
+	private static final String PASSWORD = "Admin123";
+	private final RemoteWebDriver driver;
+	private final String adminPageUrl = "http://localhost:8080/openmrs/admin";
+	private final String createProposalUrl = "/openmrs/module/cpm/concept.form";
+
+	public AdminPage(final RemoteWebDriver driver) {
+		this.driver = driver;
+		driver.get(adminPageUrl);
+		login();
+	}
+
+	/**
+	 * not really sure if this ist ja the right way to wrap selenium in a page object
+	 *
+	 * @return True if the link exists, false otherwise
+	 */
+	public boolean hasCreateProposalLink() {
+		try {
+			driver.findElement(By.cssSelector("a[href=\"" + createProposalUrl  + "\"]"));
+			return true;
+		} catch (final NoSuchElementException e) {
+			return false;
+		}
+	}
+
+	private void login() {
+		final WebElement username = driver.findElement(By.name("uname"));
+		username.sendKeys(USERNAME);
+		final WebElement password = driver.findElement(By.name("pw"));
+		password.sendKeys(PASSWORD);
+		password.submit();
+	}
+}
