be3c52494692d60e17fcdf5870dd14ca6a6a66c8
Alex Ong <aong@thoughtworks.com>
2012-08-01 20:31:23 +1000

    alex/pete: Moved functional-tests into src/main so that we can build an executable test jar added stuff to poms to build said jar

---
+++ functional-tests/src/main/java/org/openmrs/module/cpm/TestRunner.java
@@ -0,0 +1,10 @@
+package org.openmrs.module.cpm;
+
+import org.junit.runner.JUnitCore;
+
+public class TestRunner {
+
+	public static void main(String[] args) {
+		JUnitCore.main("org.openmrs.module.cpm.TestAdminPageLinks");
+	}
+}
