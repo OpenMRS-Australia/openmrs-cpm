4787b9958f14cf3a5a9605a99b727dc2c9156864
David Sanders <dsanders@rapilabs.com>
2012-09-27 22:59:01 +1000

    Added querying and row population

--- omod/src/main/webapp/angular/SearchConceptsDialogCtrl.js
+++ omod/src/main/webapp/angular/SearchConceptsDialogCtrl.js
@@ -6,7 +6,44 @@ function SearchConceptsDialogCtrl($scope) {
 
   var RETURN = 13;
 
+  var CONCEPT_LIST = [
+    {   
+      "id": 1,
+      "name": "Common cold",
+      "description": "Respiratory tract infection, upper",
+      "datatype": "Numeric",
+      "selected": false
+    },  
+    {   
+      "id": 2,
+      "name": "Pulmonary Effusion",
+      "description": "Accumulation of serous, purulent, or bloody fluid into the pulmonary cavity.",
+      "datatype": "Numeric",
+      "selected": false
+    },  
+    {   
+      "id": 3,
+      "name": "Vitamin C Deficiency",
+      "description": "A disease caused by a lack of vitamin C and characterized by spongy gums, loosening of the teeth, and bleeding into the skin and mucous membranes.",
+      "datatype": "Numeric",
+      "selected": false
+    }   
+  ];
+
   var currTimeout;
+
+  function doSearch() {
+    $scope.concepts = [];
+    var query = $scope.query;
+    for (var i in CONCEPT_LIST) {
+      var currRow = CONCEPT_LIST[i];
+      if (currRow.name.toLowerCase().indexOf(query.toLowerCase()) != -1 || currRow.description.toLowerCase().indexOf(query.toLowerCase()) != -1) {
+        $scope.$apply(function(){
+          $scope.concepts.push(CONCEPT_LIST[i]);
+        });
+      }
+    }
+  }
 
   $scope.search = function(which) {
     var found = false;
@@ -21,7 +58,7 @@ function SearchConceptsDialogCtrl($scope) {
         clearTimeout(currTimeout)
       }
       currTimeout = setTimeout(function() {
-        alert("searching after delay");
+        doSearch();
       }, SEARCH_DELAY);
 
     // trigger search immediately
@@ -29,15 +66,35 @@ function SearchConceptsDialogCtrl($scope) {
       if (typeof currTimeout != "undefined") {
         clearTimeout(currTimeout)
       }
-      alert("searching now");
+      doSearch();
     }
   };
 
   $scope.add = function() {
-    alert("add");
+    var concepts = getSelectedConcepts();
+    var string = "Adding: ";
+    for (var i in concepts) {
+      string += concepts[i].name + ", ";
+    }
+    alert(string);
   };
 
   $scope.cancel = function() {
     alert("cancel");
   };
+
+  $scope.concepts = [];
+
+  function getSelectedConcepts() {
+    var selectedList = [];
+
+    for (var i in $scope.concepts) {
+      if ($scope.concepts[i].selected) {
+        selectedList[selectedList.length] = $scope.concepts[i];
+      }
+    }
+
+    return selectedList;
+  }
+
 }
