d7e1856dded971e1cbe94bd7505b427d20de97e7
dsanders <dsanders@rapilabs.com>
2012-10-03 20:04:15 +1000

    Define DI the preferred way; also split up directive definitions into separate file

--- omod/src/main/webapp/angular/cpm.js
+++ omod/src/main/webapp/angular/cpm.js
@@ -1,41 +1,7 @@
 var cpm = angular.module('cpm', []);
 
-cpm.config(function($routeProvider){
-  $routeProvider.when('/', {controller: ListConceptProposalsCtrl, templateUrl: 'ListConceptProposals.html'});
-  $routeProvider.when('/create', {controller: CreateConceptProposalCtrl, templateUrl: 'CreateConceptProposal.html'});
-});
-
-
-cpm.directive('cpmKeyup', function() {
-  return function(scope, el, attrs) {
-    var keyupFn = scope.$eval(attrs.cpmKeyup);
-    el.bind("keyup", function(e) {
-      scope.$apply(function() {
-        keyupFn.call(scope, e.which);
-      });
-    });
-  };
-});
-
-cpm.directive('jqueryUiDialog', function () {
-    return function (scope, element, attrs) {
-        var dialog = $(element).dialog(
-        {autoOpen:false, width: 800, title: "Add concepts",
-        close:function()
-            {
-                try
-                {
-                var functionName= attrs.jqueryUiDialogClosed;
-                //alert(functionName);
-                if(typeof scope[functionName]=="function")
-                    setTimeout(function(){scope.$apply(scope[functionName]);},100);
-                }
-                catch(err)
-                {}
-            }
-        });
-        scope.$watch("dialog", function (value) {
-            $(element).dialog(value);
-        });
-    }
-});
+cpm.config(['$routeProvider', function($routeProvider){
+  $routeProvider.
+    when('/', {controller: ListConceptProposalsCtrl, templateUrl: 'ListConceptProposals.html'}).
+    when('/create', {controller: CreateConceptProposalCtrl, templateUrl: 'CreateConceptProposal.html'});
+}]);
