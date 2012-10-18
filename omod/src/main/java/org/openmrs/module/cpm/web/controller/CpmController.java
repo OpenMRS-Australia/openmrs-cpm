d68a3d63acbd9d1eb1fcba663141ebb2bf60e2cb
dsanders <dsanders@rapilabs.com>
2012-10-18 22:41:13 +1100

    Adding simple rest layer on top of concept proposal package

--- omod/src/main/java/org/openmrs/module/cpm/web/controller/CpmController.java
+++ omod/src/main/java/org/openmrs/module/cpm/web/controller/CpmController.java
@@ -1,8 +1,14 @@
 package org.openmrs.module.cpm.web.controller;
 
+import java.util.List;
+
+import org.openmrs.api.context.Context;
+import org.openmrs.module.cpm.ConceptProposalPackage;
+import org.openmrs.module.cpm.api.ConceptProposalService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class CpmController {
@@ -10,6 +16,11 @@ public class CpmController {
 	@RequestMapping(value = "module/cpm/proposals.list", method = RequestMethod.GET)
 	public String listProposals() {
 		return "/module/cpm/proposals";
+	}
+
+	@RequestMapping(value = "module/cpm/rest/proposals.list", method = RequestMethod.GET)
+	public @ResponseBody List<ConceptProposalPackage> getProposals() {
+		return Context.getService(ConceptProposalService.class).getAllConceptProposalPackages();
 	}
 
 }
