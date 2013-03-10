31745bcf1a145d4b86cc43cc434e300d3aa7dc76
David Sanders <dsanders@rapilabs.com>
2013-03-10 20:03:53 +1100

    Replacing the annoying 'Edit' link with a clickable table row

--- omod/src/main/webapp/resources/ListIncomingProposalsCtrl.js
+++ omod/src/main/webapp/resources/ListIncomingProposalsCtrl.js
@@ -1,10 +1,16 @@
 define(['cpm-review', 'config'], function(cpm, config) {
-  cpm.controller('ListIncomingProposalsCtrl', ['$scope', 'ProposalResponses', function($scope, ProposalResponses) {
+  cpm.controller('ListIncomingProposalsCtrl', ['$scope', 'ProposalResponses', '$location', function($scope, ProposalResponses, $location) {
+
     document.title = 'Incoming Concept Proposals';
     $scope.contextPath = config.contextPath;
     $scope.responseReceived = false;
+
     $scope.proposals = ProposalResponses.query(function() {
       $scope.responseReceived = true;
     });
+
+    $scope.editProposal = function(proposalId) {
+      $location.path('/edit/' + proposalId)
+    }
   }]);
 });
--- omod/src/main/webapp/resources/ListProposalsCtrl.js
+++ omod/src/main/webapp/resources/ListProposalsCtrl.js
@@ -1,10 +1,16 @@
 define(['cpm', 'config'], function(cpm, config) {
-  cpm.controller('ListProposalsCtrl', ['$scope', 'Proposals', function($scope, Proposals) {
+  cpm.controller('ListProposalsCtrl', ['$scope', 'Proposals', '$location', function($scope, Proposals, $location) {
+
     document.title = 'Manage Concept Proposals';
     $scope.contextPath = config.contextPath;
     $scope.responseReceived = false;
+
     $scope.proposals = Proposals.query(function() {
       $scope.responseReceived = true;
     });
+
+    $scope.editProposal = function(proposalId) {
+      $location.path('/edit/' + proposalId)
+    }
   }]);
 });
