dbbc7afa56d1d3eec1798625a44d9dc0f0fac385
David Sanders <dsanders@rapilabs.com>
2013-03-10 19:36:27 +1100

    Disabling the save button while in flight

--- omod/src/main/webapp/resources/SettingsCtrl.js
+++ omod/src/main/webapp/resources/SettingsCtrl.js
@@ -1,13 +1,19 @@
 define(['cpm', 'config'], function(cpm, config) {
   cpm.controller('SettingsCtrl', ['$scope', 'Settings', function($scope, Settings) {
+
     document.title = 'Manage Concept Proposal Settings';
+    $scope.saving = false;
+
     $scope.responseReceived = false;
     $scope.settings = Settings.get(function() {
       $scope.responseReceived = true;
     });
 
     $scope.save = function() {
-      $scope.settings.$save();
+      $scope.saving = true;
+      $scope.settings.$save(function() {
+        $scope.saving = false;
+      });
     }
   }]);
 });
