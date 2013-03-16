e70faa9b921d239708a0110a485d29b130a7d3e9
David Sanders <dsanders@rapilabs.com>
2013-03-16 19:44:53 +1100

    Adding review status

--- omod/src/main/webapp/resources/filters.js
+++ omod/src/main/webapp/resources/filters.js
@@ -4,15 +4,41 @@ define(['angular'], function(angular) {
 
   filters.filter('proposalStatus', function() {
     return function(input) {
-      if (input === 'DRAFT') {
-        return 'Draft';
-      } else if (input === 'SUBMITTED') {
-        return 'Submitted';
-      } else {
-        return '';
+      switch (input) {
+
+        case 'DRAFT':
+          return 'Draft';
+
+        case 'SUBMITTED':
+          return 'Submitted';
+
+        default:
+          return '';
       }
     };
   });
+
+  filters.filter('proposalReviewStatus', function() {
+    return function(input) {
+      switch (input) {
+
+        case 'RECEIVED':
+          return 'Open';
+
+        case 'CLOSED_NEW':
+          return 'New';
+
+        case 'CLOSED_EXISTING':
+          return 'Existing';
+
+        case 'CLOSED_REJECTED':
+          return 'Rejected';
+
+        default:
+          return '';
+      }
+    }
+  })
 
   return filters;
 });
