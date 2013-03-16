a2188b54fa69b1aae480022e069a4b73f25eadf9
David Sanders <dsanders@rapilabs.com>
2013-03-16 18:54:01 +1100

    Forgot to add @Controller

--- omod/src/main/java/org/openmrs/module/cpm/web/controller/ReviewController.java
+++ omod/src/main/java/org/openmrs/module/cpm/web/controller/ReviewController.java
@@ -6,6 +6,7 @@ import org.openmrs.module.cpm.ProposedConceptResponsePackage;
 import org.openmrs.module.cpm.api.ProposedConceptService;
 import org.openmrs.module.cpm.web.dto.ProposedConceptDto;
 import org.openmrs.module.cpm.web.dto.ProposedConceptResponsePackageDto;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -14,6 +15,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 
+@Controller
 public class ReviewController {
 
 	//
