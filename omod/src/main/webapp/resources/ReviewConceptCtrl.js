a86f37bfd990f3a56a9c0d87fb31e7bc0416ab09
David Sanders <dsanders@rapilabs.com>
2013-03-17 22:00:49 +1100

    Bound concept to scope

--- omod/src/main/webapp/resources/ReviewConceptCtrl.js
+++ omod/src/main/webapp/resources/ReviewConceptCtrl.js
@@ -2,11 +2,13 @@ define(['cpm-review', 'config'], function(module, config) {
     module.controller('ReviewConceptCtrl', ['$scope', '$routeParams', 'ProposalReviews', function($scope, $routeParams, ProposalReviews) {
 
         var proposalId = $routeParams.proposalId;
+        var conceptId = $routeParams.conceptId;
         $scope.isLoading = true;
         $scope.resourceLocation = config.resourceLocation;
 
         $scope.proposal = ProposalReviews.get({proposalId: proposalId}, function() {
             $scope.isLoading = false;
+            $scope.concept = $scope.proposal.concepts[conceptId];
         });
 
         $scope.showConcept = function(conceptId) {
