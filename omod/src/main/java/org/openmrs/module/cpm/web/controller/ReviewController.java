6284440773dc6bd08d154d1bcb3ac8320e34a508
Chanaka <chanaka.wijesena@gmail.com>
2013-04-05 10:02:11 +1100

    Changes to add reviewer comment to concept proposal response and save it in DB

--- omod/src/main/java/org/openmrs/module/cpm/web/controller/ReviewController.java
+++ omod/src/main/java/org/openmrs/module/cpm/web/controller/ReviewController.java
@@ -83,7 +83,7 @@ public class ReviewController {
 		final ProposedConceptResponse proposedConcept = aPackage.getProposedConcept(conceptId);
 		if (proposedConcept != null) {
 			// todo: comment
-            proposedConcept.setComment(updatedProposalResponse.getComment());
+            proposedConcept.setReviewerComment(updatedProposalResponse.getComment());
 			proposedConcept.setStatus(updatedProposalResponse.getStatus());
 			service.saveProposedConceptResponsePackage(aPackage);
 		}
