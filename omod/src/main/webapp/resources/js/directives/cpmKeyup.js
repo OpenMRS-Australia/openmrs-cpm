7c5acd9b0304e995b736f9f94cfb52e2ccab7bb9
Kent McNeill <imxerxes@hotmail.com>
2013-05-22 23:08:32 +1000

    break up directives into separate files

---
+++ omod/src/main/webapp/resources/js/directives/cpmKeyup.js
@@ -0,0 +1,21 @@
+define(['angular', 'jquery', 'jquery-ui'], function(angular, $) {
+
+  var directives;
+  try {
+      directives = angular.module('cpm.directives');
+  }
+  catch(err) {
+    directives = angular.module("cpm.directives", []);
+  }
+
+	directives.directive('cpmKeyup', function() {
+    	return function(scope, el, attrs) {
+      		var keyupFn = scope.$eval(attrs.cpmKeyup);
+      		el.bind("keyup", function(e) {
+        		scope.$apply(function() {
+          			keyupFn.call(scope, e.which);
+        		});
+      		});
+    	};
+  	});
+});
