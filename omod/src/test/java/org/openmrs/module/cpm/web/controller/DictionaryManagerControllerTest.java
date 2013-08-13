6f3dc163eeb9d69b2f8b272bb00c4316c0044812
David Sanders <dsanders@rapilabs.com>
2013-08-13 22:21:04 +1000

    Added test for numeric proposals

--- omod/src/test/java/org/openmrs/module/cpm/web/controller/DictionaryManagerControllerTest.java
+++ omod/src/test/java/org/openmrs/module/cpm/web/controller/DictionaryManagerControllerTest.java
@@ -10,11 +10,14 @@ import org.openmrs.ConceptDatatype;
 import org.openmrs.api.ConceptService;
 import org.openmrs.api.context.Context;
 import org.openmrs.module.cpm.ProposedConceptResponse;
+import org.openmrs.module.cpm.ProposedConceptResponseNumeric;
 import org.openmrs.module.cpm.ProposedConceptResponsePackage;
 import org.openmrs.module.cpm.api.ProposedConceptService;
 import org.openmrs.module.cpm.web.dto.SubmissionDto;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
+
+import java.io.IOException;
 
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -35,6 +38,9 @@ public class DictionaryManagerControllerTest {
 	private ConceptClass conceptClassMock;
 
 	@Mock
+	private ProposedConceptResponseNumeric responseNumericMock;
+
+	@Mock
 	private ConceptService conceptServiceMock;
 
 	@Mock
@@ -46,8 +52,14 @@ public class DictionaryManagerControllerTest {
 	private DictionaryManagerController controller;
 
 	@Before
-	public void before() {
+	public void before() throws Exception {
 		controller = new DictionaryManagerController();
+
+		mockStatic(Context.class);
+		when(Context.getConceptService()).thenReturn(conceptServiceMock);
+		when(Context.getService(ProposedConceptService.class)).thenReturn(proposedConceptServiceMock);
+		whenNew(ProposedConceptResponsePackage.class).withNoArguments().thenReturn(proposedConceptResponsePackageMock);
+		whenNew(ProposedConceptResponse.class).withNoArguments().thenReturn(responseMock);
 	}
 
 	@Test
@@ -65,18 +77,10 @@ public class DictionaryManagerControllerTest {
 	}
 
 	private void setupRegularFixtureMocks() throws Exception {
-		mockStatic(Context.class);
-		when(Context.getConceptService()).thenReturn(conceptServiceMock);
-		when(conceptServiceMock.getConceptDatatypeByUuid("some datatype")).thenReturn(dataTypeMock);
-		when(conceptServiceMock.getConceptClassByUuid("blah")).thenReturn(conceptClassMock);
+		when(conceptServiceMock.getConceptDatatypeByUuid("datatype-uuid")).thenReturn(dataTypeMock);
+		when(conceptServiceMock.getConceptClassByUuid("concept-class-uuid")).thenReturn(conceptClassMock);
 		when(dataTypeMock.getUuid()).thenReturn("uuid!");
-
-		when(Context.getService(ProposedConceptService.class)).thenReturn(proposedConceptServiceMock);
-
-		whenNew(ProposedConceptResponsePackage.class).withNoArguments().thenReturn(proposedConceptResponsePackageMock);
 		when(proposedConceptResponsePackageMock.getId()).thenReturn(1);
-
-		whenNew(ProposedConceptResponse.class).withNoArguments().thenReturn(responseMock);
 	}
 
 	private SubmissionDto setupRegularProposalFixtureWithJson() throws Exception {
@@ -89,8 +93,8 @@ public class DictionaryManagerControllerTest {
 				"  'concepts': [" +
 				"    {" +
 				"      'uuid': 'concept-uuid'," +
-				"      'conceptClass': 'blah'," +
-				"      'datatype': 'some datatype'," +
+				"      'conceptClass': 'concept-class-uuid'," +
+				"      'datatype': 'datatype-uuid'," +
 				"      'comment': 'some comment'," +
 				"      'names': [" +
 				"        {" +
@@ -110,5 +114,75 @@ public class DictionaryManagerControllerTest {
 
 		ObjectMapper mapper = new ObjectMapper();
 		return mapper.readValue(regularFixture.replace("'", "\""), SubmissionDto.class);
+	}
+
+	@Test
+	public void submitProposal_numericProposal_shouldPersistDetails() throws Exception {
+		final SubmissionDto dto = setupNumericProposalFixture();
+		setupRegularFixtureMocks();
+		setupNumericFixtureMocks();
+
+		controller.submitProposal(dto);
+
+		verify(responseMock).setConceptClass(conceptClassMock);
+		verify(responseMock).setComment("some comment");
+		verify(responseMock).setProposedConceptUuid("concept-uuid");
+		verify(responseMock).setDatatype(dataTypeMock);
+		verify(responseMock).setConceptClass(conceptClassMock);
+
+		verify(responseNumericMock).setUnits("ml");
+		verify(responseNumericMock).setHiNormal(100.5);
+		verify(responseNumericMock).setHiCritical(110.0);
+		verify(responseNumericMock).setHiAbsolute(1000.0);
+		verify(responseNumericMock).setLowNormal(20.3);
+		verify(responseNumericMock).setLowCritical(15.0);
+		verify(responseNumericMock).setLowAbsolute(0.0);
+	}
+
+	private SubmissionDto setupNumericProposalFixture() throws IOException {
+		final String fixture =
+				"{" +
+				"  'name': 'A proposal'," +
+				"  'email': 'asdf@asdf.com'," +
+				"  'description': 'A description'," +
+				"  'concepts': [" +
+				"    {" +
+				"      'uuid': 'concept-uuid'," +
+				"      'conceptClass': 'concept-class-uuid'," +
+				"      'datatype': '8d4a4488-c2cc-11de-8d13-0010c6dffd0f'," +
+				"      'comment': 'some comment'," +
+				"      'names': [" +
+				"        {" +
+				"          'name': 'Concept name'," +
+				"          'locale': 'en'" +
+				"        }" +
+				"      ]," +
+				"      'descriptions': [" +
+				"        {" +
+				"          'description': 'Concept description'," +
+				"          'locale': 'en'" +
+				"        }" +
+				"      ]," +
+				"      'numericDetails': {" +
+				"        'units': 'ml'," +
+				"        'precise': true," +
+				"        'hiNormal': 100.5," +
+				"        'hiCritical': 110," +
+				"        'hiAbsolute': 1000," +
+				"        'lowNormal': 20.3," +
+				"        'lowCritical': 15," +
+				"        'lowAbsolute': 0" +
+				"      }" +
+				"    }" +
+				"  ]" +
+				"}";
+		ObjectMapper mapper = new ObjectMapper();
+		return mapper.readValue(fixture.replace("'", "\""), SubmissionDto.class);
+	}
+
+	private void setupNumericFixtureMocks() throws Exception {
+		when(conceptServiceMock.getConceptDatatypeByUuid("8d4a4488-c2cc-11de-8d13-0010c6dffd0f")).thenReturn(dataTypeMock);
+		when(dataTypeMock.getUuid()).thenReturn("8d4a4488-c2cc-11de-8d13-0010c6dffd0f");
+		whenNew(ProposedConceptResponseNumeric.class).withNoArguments().thenReturn(responseNumericMock);
 	}
 }
