380e423f923f7a1aa3b3435c5921647818c12220
cathy-mcdonald <github.cathy_mcdonald@spamgourmet.com>
2014-03-09 08:47:28 +1100

    Add checkUrl function to test url

--- conceptpropose/omod/src/main/webapp/resources/js/controllers/SettingsCtrl.js
+++ conceptpropose/omod/src/main/webapp/resources/js/controllers/SettingsCtrl.js
@@ -5,24 +5,35 @@ define([
     'js/services/menu',
     './index'
   ],
-  function(angular, config) {
+  function(angular, config, $http) {
 
     'use strict';
   
     angular.module('conceptpropose.controllers').controller('SettingsCtrl',
-      function($scope, Settings, Menu) {
+      function($scope, $http, Settings, Menu) {
 
         $scope.contextPath = config.contextPath;
         $scope.resourceLocation = config.resourceLocation;
 
         document.title = 'Manage Concept Proposal Settings';
         $scope.isLoading = true;
+        $scope.isTesting = false;
 
         $scope.menu = Menu.getMenu(3);
 
         $scope.settings = Settings.get(function() {
           $scope.isLoading = false;
         });
+
+        $scope.checkUrl=function(){
+            $scope.isTesting = true;
+            $http.post(config.contextPath + '/ws/conceptpropose/settings/url', $scope.settings.url)
+                 .success(
+                    function(isUrlInvalid){
+                       $scope.urlInvalid = isUrlInvalid
+                       $scope.isTesting = false;
+                  });
+        };
 
         $scope.save = function() {
           $scope.isLoading = true;
