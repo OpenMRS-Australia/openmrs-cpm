cfc522de9c0465e9852ecb781732d331749aa693
cathy-mcdonald <github.cathy_mcdonald@spamgourmet.com>
2014-03-10 20:16:14 +1100

    Separate REST service for testing connection with supplied settings

--- conceptpropose/omod/src/main/java/org/openmrs/module/conceptpropose/web/controller/CpmSettingsController.java
+++ conceptpropose/omod/src/main/java/org/openmrs/module/conceptpropose/web/controller/CpmSettingsController.java
@@ -1,36 +1,38 @@
 package org.openmrs.module.conceptpropose.web.controller;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.Credentials;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.params.BasicHttpParams;
+import org.apache.http.params.HttpConnectionParams;
+import org.apache.http.params.HttpParams;
 import org.openmrs.GlobalProperty;
 import org.openmrs.api.AdministrationService;
 import org.openmrs.api.context.Context;
-import org.openmrs.module.conceptpropose.web.authentication.factory.AuthHttpHeaderFactory;
 import org.openmrs.module.conceptpropose.web.common.CpmConstants;
 import org.openmrs.module.conceptpropose.web.dto.Settings;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.client.RestOperations;
+import org.directwebremoting.util.Logger;
+import java.io.IOException;
 
 @Controller
 public class CpmSettingsController {
-	private final RestOperations submissionRestTemplate;
+    private final Logger log = Logger.getLogger(CpmSettingsController.class);
 
-	private final AuthHttpHeaderFactory httpHeaderFactory;
+    private final int httpConnectionTimeout = 10000;
+    private final int httpSocketTimeout = 10000;
 
-
-    @Autowired
-    public CpmSettingsController(final RestOperations submissionRestTemplate,
-                          final AuthHttpHeaderFactory httpHeaderFactory) {
-        this.submissionRestTemplate = submissionRestTemplate;
-        this.httpHeaderFactory = httpHeaderFactory;
+    public CpmSettingsController() {
     }
-	
+
     @RequestMapping(value = "/conceptpropose/settings", method = RequestMethod.GET)
     public @ResponseBody Settings getSettings() {
         AdministrationService service = Context.getAdministrationService();
@@ -38,7 +40,6 @@ public class CpmSettingsController {
         settings.setUrl(service.getGlobalProperty(CpmConstants.SETTINGS_URL_PROPERTY));
         settings.setUsername(service.getGlobalProperty(CpmConstants.SETTINGS_USER_NAME_PROPERTY));
         settings.setPassword(service.getGlobalProperty(CpmConstants.SETTINGS_PASSWORD_PROPERTY));
-        settings.setUrlInvalid(checkSettingsUrlInvalid());
         return settings;
     }
 
@@ -48,25 +49,42 @@ public class CpmSettingsController {
         service.saveGlobalProperty(new GlobalProperty(CpmConstants.SETTINGS_URL_PROPERTY, settings.getUrl()));
         service.saveGlobalProperty(new GlobalProperty(CpmConstants.SETTINGS_USER_NAME_PROPERTY, settings.getUsername()));
         service.saveGlobalProperty(new GlobalProperty(CpmConstants.SETTINGS_PASSWORD_PROPERTY, settings.getPassword()));
-        settings.setUrlInvalid(checkSettingsUrlInvalid());
         return settings;
     }
-    
-    private boolean checkSettingsUrlInvalid() {
-    	AdministrationService service = Context.getAdministrationService();
 
-		HttpHeaders headers = httpHeaderFactory.create(
-				service.getGlobalProperty(CpmConstants.SETTINGS_USER_NAME_PROPERTY),
-                service.getGlobalProperty(CpmConstants.SETTINGS_PASSWORD_PROPERTY)
-        );
+    // Using Apache HttpComponents because OpenMRS includes Spring 3.0.5 without
+    // Jakarta Commons HttpClient and so cannot set timeout using Spring RestTemplate
+    @RequestMapping(value = "/conceptpropose/settings/connectionResult", method = RequestMethod.POST)
+    public @ResponseBody boolean testConnection(@RequestBody Settings settings) {
+        Credentials credentials = new UsernamePasswordCredentials(
+                settings.getUsername(),
+                settings.getPassword());
 
-		final String url = service.getGlobalProperty(CpmConstants.SETTINGS_URL_PROPERTY) + "/ws/conceptpropose/settings";
-		try {
-			ResponseEntity<Settings> responseEntity = submissionRestTemplate.getForEntity(url, Settings.class, headers);
-			return responseEntity.getStatusCode() != HttpStatus.OK;
-		}
-		catch (Exception ex) {
-			return true;
-		}
+        final String url = settings.getUrl() + "/ws/conceptreview/dictionarymanager/status";
+
+        HttpParams params = new BasicHttpParams();
+        HttpConnectionParams.setConnectionTimeout(params, httpConnectionTimeout);
+        HttpConnectionParams.setSoTimeout(params, httpSocketTimeout);
+
+        boolean connectionSucceeded = false;
+
+        try {
+            DefaultHttpClient httpclient = new DefaultHttpClient(params);
+            httpclient.getCredentialsProvider().setCredentials(AuthScope.ANY, credentials);
+            HttpGet httpget = new HttpGet(url);
+            HttpResponse response = httpclient.execute(httpget);
+            int statusCode = response.getStatusLine().getStatusCode();
+            connectionSucceeded = (statusCode == HttpStatus.SC_OK);
+
+            if (!connectionSucceeded) {
+                log.error("Test " + url + " failed with status code " + statusCode);
+            }
+
+            return connectionSucceeded;
+        }
+        catch (IOException ex) {
+            log.error("Test " + url + " failed with exception {" + ex.getMessage() + "}");
+            return connectionSucceeded;
+        }
     }
 }
