01ee8a23917140d78f853c19367250e53a6f41aa
David Sanders <dsanders@rapilabs.com>
2014-04-14 22:07:54 +1000

    Added missing angular filters

--- conceptreview/omod/src/main/webapp/resources/js/impl.js
+++ conceptreview/omod/src/main/webapp/resources/js/impl.js
@@ -2,7 +2,9 @@ define([
     'js/controllers/ListIncomingProposalsCtrl',
     'js/controllers/ReviewProposalCtrl',
     'js/controllers/ReviewConceptCtrl',
-    'js/filters/proposalReviewStatus'
+    'js/filters/proposalReviewStatus',
+    'js/filters/dashOnNull',
+    'js/filters/boolToYesOrNo'
   ],
   function () {
     'use strict';
