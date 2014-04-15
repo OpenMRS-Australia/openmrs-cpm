7097f6b8bc1e55668a3c56fb61b1292bdfb11d05
David Sanders <dsanders@rapilabs.com>
2014-04-15 19:46:15 +1000

    Added missing directive

--- conceptreview/omod/src/main/webapp/resources/js/impl.js
+++ conceptreview/omod/src/main/webapp/resources/js/impl.js
@@ -2,6 +2,7 @@ define([
     'js/controllers/ListIncomingProposalsCtrl',
     'js/controllers/ReviewProposalCtrl',
     'js/controllers/ReviewConceptCtrl',
+    'js/directives/conceptReviewMenu',
     'js/filters/proposalReviewStatus',
     'js/filters/dashOnNull',
     'js/filters/boolToYesOrNo'
