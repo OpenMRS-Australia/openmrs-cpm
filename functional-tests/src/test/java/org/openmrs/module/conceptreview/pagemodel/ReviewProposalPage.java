a810398dd6073cea8d2cf844d1f994724e2cefa1
David Sanders <dsanders@rapilabs.com>
2014-05-03 19:18:40 +1000

    Creating page models for review pages

---
+++ functional-tests/src/test/java/org/openmrs/module/conceptreview/pagemodel/ReviewProposalPage.java
@@ -0,0 +1,28 @@
+package org.openmrs.module.conceptreview.pagemodel;
+
+import org.openmrs.module.conceptpropose.pagemodel.BaseCpmPage;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import java.util.HashMap;
+
+public class ReviewProposalPage extends BaseCpmPage {
+
+	public ReviewProposalPage(WebDriver driver) {
+		super(driver);
+	}
+
+	public HashMap<String, String> getConcepts() {
+		HashMap<String, String> concepts = new HashMap<String, String>();
+		for (WebElement element : driver.findElements(By.cssSelector(".concept"))) {
+			concepts.put("name", element.findElement(By.className("name")).getText());
+			concepts.put("class", element.findElement(By.className("class")).getText());
+			concepts.put("datatype", element.findElement(By.className("datatype")).getText());
+			concepts.put("comments", element.findElement(By.className("comments")).getText());
+			concepts.put("status", element.findElement(By.className("status")).getText());
+		}
+
+		return concepts;
+	}
+}
